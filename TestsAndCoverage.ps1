a657045270c41e89ae5c82ead1c686c05fd918ab
scafeman <matt.scafe@rackspace.com>
2019-03-27 00:37:00 -0500

    changed dotnetArguments from xunit to test

--- TestsAndCoverage.ps1
+++ TestsAndCoverage.ps1
@@ -2,10 +2,10 @@ $testProjects = "XUnitTest", "XUnitTest.Integration"
 
 # Get the most recent OpenCover NuGet package from the dotnet nuget packages
 $nugetOpenCoverPackage = Join-Path -Path $env:C:\Users\scafe -ChildPath "\.nuget\packages\opencover\4.7.922"
-$latestOpenCover = Join-Path -Path ((Get-ChildItem -Path $nugetOpenCoverPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "..\tools\OpenCover.Console.exe"
+$latestOpenCover = Join-Path -Path ((Get-ChildItem -Path $nugetOpenCoverPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath ".\tools\OpenCover.Console.exe"
 # Get the most recent OpenCoverToCoberturaConverter from the dotnet nuget packages
 $nugetCoberturaConverterPackage = Join-Path -Path $env:C:\Users\scafe -ChildPath "\.nuget\packages\OpenCoverToCoberturaConverter"
-$latestCoberturaConverter = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "..\tools\OpenCoverToCoberturaConverter.exe"
+$latestCoberturaConverter = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "\tools\OpenCoverToCoberturaConverter.exe"
 
 If (Test-Path "$PSScriptRoot\RazorPagesMovie\$testProject\OpenCover.coverageresults"){
 	Remove-Item "$PSScriptRoot\RazorPagesMovie\$testProject\OpenCover.coverageresults"
@@ -20,7 +20,7 @@ If (Test-Path "$PSScriptRoot\RazorPagesMovie\$testProject\Cobertura.coverageresu
 $testRuns = 1;
 foreach ($testProject in $testProjects){
     # Arguments for running dotnet
-    $dotnetArguments = "xunit", "-xml `"`"$PSScriptRoot\RazorPagesMovie\$testProject\testRuns_$testRuns.testresults`"`""
+    $dotnetArguments = "test", "-xml `"`"$PSScriptRoot\RazorPagesMovie\$testProject\testRuns_$testRuns.testresults`"`""
 
     "Running tests with OpenCover"
     & $latestOpenCover `
